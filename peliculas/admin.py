c35aa5a08746cd86ef8e2261c32885e8f2fb80cd
Miguel Angel Rios <riosmiguelangel@yahoo.com.ar>
2023-04-24 22:44:58 -0300

    con conexion a la BD

--- peliculas/admin.py
+++ peliculas/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Peliculas
 from .models import Generos
+from .models import Artistas
+from .models import FailedJobs
+from .models import Migrations
+from .models import PasswordResets
+from .models import Users
 
 admin.site.register(Peliculas)
 admin.site.register(Generos)
+admin.site.register(Artistas)
+admin.site.register(FailedJobs)
+admin.site.register(Migrations)
+admin.site.register(PasswordResets)
+admin.site.register(Users)
